b5f9620761059f1662a072025dc203f6e6642486
Larry Ludlow <madbuda@gmail.com>
2017-08-18 12:07:03 -0400

    Update index.js

--- bitcore-node-hush/index.js
+++ bitcore-node-hush/index.js
@@ -9,7 +9,7 @@ var InsightUI = function(options) {
   if (typeof options.apiPrefix !== 'undefined') {
     this.apiPrefix = options.apiPrefix;
   } else {
-    this.apiPrefix = 'insight-api-zcash';
+    this.apiPrefix = 'insight-api-hush';
   }
   if (typeof options.routePrefix !== 'undefined') {
     this.routePrefix = options.routePrefix;
@@ -18,7 +18,7 @@ var InsightUI = function(options) {
   }
 };
 
-InsightUI.dependencies = ['insight-api-zcash'];
+InsightUI.dependencies = ['insight-api-hush'];
 
 inherits(InsightUI, BaseService);
 
